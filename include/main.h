7fa78f98e247c30b76389894bf7ea06d6749f431
hwyz_leo <hwyz.leo.ye@gmail.com>
2025-05-21 16:55:32 +0800

    改进: 优化初始化日志

--- include/main.h
+++ include/main.h
@@ -1,8 +1,14 @@
 //
-// Created by 叶荣杰 on 2024/9/5.
+// Created by hwyz_leo on 2024/9/5.
 //
 
 #ifndef TSPSERVICE_MAIN_H
 #define TSPSERVICE_MAIN_H
 
 #endif //TSPSERVICE_MAIN_H
+
+/**
+ * 初始化日志
+ * @param config 配置
+ */
+void initLogger(const YAML::Node& config);
--- src/main.cpp
+++ src/main.cpp
@@ -20,11 +20,27 @@ int main() {
     // 加载配置
     std::string filePath = CommonFunction::getConfigFilePath();
     YAML::Node config = YAML::LoadFile(filePath);
-    // 配置日志
-    std::string log_type = config["log"]["type"].as<std::string>();
-    if (log_type == "file") {
-        std::string log_path = "./log.txt";
-        auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(log_path, true);
+    // 初始化日志
+    initLogger(config);
+
+    spdlog::info("主函数运行");
+
+    // 加载TSP MQTT配置
+    TspMqttConfig::GetInstance().LoadConfig(config);
+    // 启动TSP MQTT客户端
+    TspMqttClient::GetInstance().Start();
+    // 启动TBOX MQTT客户端
+    TboxMqttClient::GetInstance().Start();
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+    }
+}
+
+void initLogger(const YAML::Node& config) {
+    std::string logger_type = config["logger"]["type"].as<std::string>();
+    if (logger_type == "file") {
+        std::string logger_path = config["logger"]["path"].as<std::string>();
+        auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logger_path, true);
         file_sink->set_level(spdlog::level::debug);
         auto logger = std::make_shared<spdlog::logger>("file_logger", file_sink);
         logger->set_level(spdlog::level::debug);
@@ -37,15 +53,4 @@ int main() {
         logger->set_level(spdlog::level::debug);
         spdlog::set_default_logger(logger);
     }
-    spdlog::info("主函数运行");
-    // 加载TSP MQTT配置
-    TspMqttConfig::GetInstance().LoadConfig(config);
-    // 启动TSP MQTT客户端
-    TspMqttClient::GetInstance().Start();
-    // 启动TBOX MQTT客户端
-    TboxMqttClient::GetInstance().Start();
-    while (true) {
-        std::this_thread::sleep_for(std::chrono::seconds(10));
-    }
 }
-
